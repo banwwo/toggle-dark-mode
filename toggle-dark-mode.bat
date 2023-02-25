285fa815e10b8ac64b76c8e1d087954a0d2fc23b
Esteban <56206284+banwwo@users.noreply.github.com>
2023-02-25 03:24:08 -0300

    Toggle Taskbar mode
    Now you can toggle dark mode or light mode in the taskbar.

--- toggle-dark-mode.bat
+++ toggle-dark-mode.bat
@@ -21,3 +21,19 @@ if "%theme%"=="2" (
     reg add %path_us% /v AppsUseLightTheme /t REG_DWORD /d 0 /f /reg:32
     reg add %path_mch% /v AppsUseLightTheme /t REG_DWORD /d 0 /f /reg:32
 )
+
+cls
+
+echo.
+echo  [1] Sí, quiero usar el Dark Mode
+echo  [2] No, prefiero usar el Light Mode
+echo.
+set /p taskbar=" ¿Deseas que el Taskbar use el Dark Mode? 1/2 -> "
+
+
+if "%taskbar%"=="1" (
+    reg add %path_us% /v SystemUsesLightTheme /t REG_DWORD /d 0 /f /reg:32
+)
+if "%taskbar%"=="2" (
+    reg add %path_us% /v SystemUsesLightTheme /t REG_DWORD /d 1 /f /reg:32
+)
